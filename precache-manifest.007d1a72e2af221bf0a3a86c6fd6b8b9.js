d62db455deb0b4ac331591d9439b72d7c1313557
csabat <toth.csaba1988@gmail.com>
2018-12-14 02:39:08 +0200

    Updates

---
+++ precache-manifest.007d1a72e2af221bf0a3a86c6fd6b8b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a68601f97b9adb681be9",
+    "url": "/exel-parser/static/js/runtime~main.a68601f9.js"
+  },
+  {
+    "revision": "fc7fa7bfd332cfb4f6ab",
+    "url": "/exel-parser/static/js/main.fc7fa7bf.chunk.js"
+  },
+  {
+    "revision": "81861e36f296cf866ac9",
+    "url": "/exel-parser/static/js/1.81861e36.chunk.js"
+  },
+  {
+    "revision": "fc7fa7bfd332cfb4f6ab",
+    "url": "/exel-parser/static/css/main.1c710cf1.chunk.css"
+  },
+  {
+    "revision": "5ab1f0b87c97b7486f2cdb359dc93887",
+    "url": "/exel-parser/index.html"
+  }
+];
